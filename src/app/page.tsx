2a39ac688c61547ab4046d6345d3061e153b0e98
Kumampet <kumampet@gmail.com>
2024-03-07 23:05:44 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <React.Fragment>
-      
+
     </React.Fragment>
   );
 }
