a707a4ebe32dd9f732b892bdd3f241a94a854975
Kumampet <kumampet@gmail.com>
2024-03-09 04:22:56 +0900

    ESlintルール調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,51 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	'env': {
+		'browser': true,
+		'es2021': true
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended"
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended'
 	],
-	"overrides": [
+	'overrides': [
 		{
-			"env": {
-				"node": true
+			'env': {
+				'node': true
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
+			'files': [
+				'.eslintrc.{js,cjs}'
 			],
-			"parserOptions": {
-				"sourceType": "script"
+			'parserOptions': {
+				'sourceType': 'script'
 			}
 		}
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
 	},
-	"plugins": [
-		"@typescript-eslint",
-		"react"
+	'plugins': [
+		'@typescript-eslint',
+		'react'
 	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
 		],
-		"linebreak-style": [
-			"error",
-			"unix"
+		'linebreak-style': [
+			'error',
+			'unix'
 		],
-		"quotes": [
-			"error",
-			"double"
+		'quotes': [
+			'error',
+			'single'
 		],
-		"semi": [
-			"error",
-			"always"
+		'semi': [
+			'error',
+			'always'
 		]
 	}
 };
